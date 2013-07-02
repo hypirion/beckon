17dcbe24df2b6d0fd2c9551c1324ce803f6130a0
Jean Niklas L'orange <jeannikl@hypirion.com>
2013-07-02 03:59:34 +0200

    Fix project.clj and source paths.

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject beckon "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Handle POSIX signals in Clojure with grace and style."
+  :url "https://github.com/hyPiRion/com.hypirion.io"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :source-paths ["src/clojure"]
+  :java-source-paths ["src/java"]
   :dependencies [[org.clojure/clojure "1.5.1"]])
