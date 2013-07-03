e368f91d013cacc23bcc22ceb84f7b54bac671f6
Jean Niklas L'orange <jeannikl@hypirion.com>
2013-07-03 18:26:34 +0200

    SignalFolder implements SignalHandler.

--- src/java/com/hypirion/beckon/SignalFolder.java
+++ src/java/com/hypirion/beckon/SignalFolder.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 import java.util.concurrent.Callable;
 
-public class SignalFolder {
+public class SignalFolder implements SignalHandler {
     final private Callable[] fns;
 
     public SignalFolder(List funs) {
@@ -16,5 +16,19 @@ public class SignalFolder {
             fns[i] = (Callable) funs.get(i);
         }
     }
-    
+
+    public void handle(Signal sig) {
+        for (Callable c : fns) {
+            boolean res = false;
+            try {
+                res = (Boolean) c.call(); // No input elems?
+            }
+            catch (Exception e) {}
+            finally {
+                if (!res) {
+                    break;
+                }
+            }
+        }
+    }
 }
