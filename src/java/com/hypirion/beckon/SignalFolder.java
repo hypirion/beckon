2db024031c064de9a77b70e435b78e2dc81a59fd
Jean Niklas L'orange <jeannikl@hypirion.com>
2013-07-03 18:23:57 +0200

    Add in signalfolder ctor.

--- src/java/com/hypirion/beckon/SignalFolder.java
+++ src/java/com/hypirion/beckon/SignalFolder.java
@@ -1,5 +1,20 @@
 package com.hyprion.beckon;
 
+import sun.misc.Signal;
+import sun.misc.SignalHandler;
+
+import java.util.List;
+
+import java.util.concurrent.Callable;
+
 public class SignalFolder {
+    final private Callable[] fns;
+
+    public SignalFolder(List funs) {
+        fns = new Callable[funs.size()];
+        for (int i = 0; i < fns.length; i++) {
+            fns[i] = (Callable) funs.get(i);
+        }
+    }
     
 }
