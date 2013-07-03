a9e135ebc2c083b826477d62644b7877d7922fbb
Jean Niklas L'orange <jeannikl@hypirion.com>
2013-07-03 18:55:18 +0200

    First pass at registering method.

--- src/java/com/hypirion/beckon/SignalRegistererHelper.java
+++ src/java/com/hypirion/beckon/SignalRegistererHelper.java
@@ -1,5 +1,16 @@
 package com.hypirion.beckon;
 
-class SignalRegistererHelper {
-    
+import sun.misc.Signal;
+import sun.misc.SignalHandler;
+
+import java.util.List;
+
+public class SignalRegistererHelper {
+
+    static SignalHandler register(String signame, List fns) {
+        Signal sig = new Signal(signame);
+        SignalFolder folder = new SignalFolder(fns);
+        SignalHandler oldHandler = Signal.handle(sig, folder);
+        return oldHandler;
+    }
 }
