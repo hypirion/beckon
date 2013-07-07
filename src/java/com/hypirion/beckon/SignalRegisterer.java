963ee5d6fe09e8f8c1f467bcf5b909de10717691
Jean Niklas L'orange <jeannikl@hypirion.com>
2013-07-07 12:49:10 +0200

    First attempt at wrapper upcoming.

--- src/java/com/hypirion/beckon/SignalRegisterer.java
+++ src/java/com/hypirion/beckon/SignalRegisterer.java
@@ -1,5 +1,15 @@
 package com.hypirion.beckon;
 
+import java.util.List;
+
 public class SignalRegisterer {
-    
+    public static void setHandlerList(String signame, List fns)
+        throws SignalHandlerNotFoundException{
+        try {
+            SignalRegistererHelper.register(signame, fns);
+        }
+        catch (LinkageError le) {
+            throw new SignalHandlerNotFoundException();
+        }
+    }
 }
