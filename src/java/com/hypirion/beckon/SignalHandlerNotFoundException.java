b4e6d18c2eed17130581ce4573236709ee975e8b
Jean Niklas L'orange <jeannikl@hypirion.com>
2013-07-07 12:27:39 +0200

    Add in useful error msg on linkage errors.

--- src/java/com/hypirion/beckon/SignalHandlerNotFoundException.java
+++ src/java/com/hypirion/beckon/SignalHandlerNotFoundException.java
@@ -1,5 +1,9 @@
 package com.hypirion.beckon;
 
 class SignalHandlerNotFoundException extends Exception {
-    
+    public SignalHandlerNotFoundException() {
+        super("SignalHandler was not found on this JVM -- please report an" +
+              "issue at beckon's github page with which JVM and what version" +
+              "you're using.");
+    }
 }
