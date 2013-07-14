f087eed5c19c3b39cde422b22debc835d6e1bfd9
Jean Niklas L'orange <jeannikl@hypirion.com>
2013-07-14 21:27:58 +0200

    Make the actual interface of Beckon.

--- src/clojure/beckon.clj
+++ src/clojure/beckon.clj
@@ -1,20 +1,25 @@
 (ns beckon
-  (:import [com.hypirion.beckon SignalAtoms]
-           [sun.misc Signal]))
+  (:import (com.hypirion.beckon SignalAtoms SignalRegisterer)))
 
 (defn signal-atom [signal-name]
   (SignalAtoms/getSignalAtom signal-name))
 
 (defn raise! [signal-name]
-  (Signal/raise (Signal. signal-name)))
+  (SignalRegisterer/raiseSignal signal-name))
 
-(defn true!
+(defn re-init! [signal-name]
+  (SignalRegisterer/resetDefaultHandler signal-name))
+
+(defn re-init-all! []
+  (SignalRegisterer/resetAllHandlers))
+
+(defn always-true
   "Takes a function of no arguments, and returns a function taking no arguments
   which calls f and returns true. f has presumably side effects."
   [f]
   (fn [] (f) true))
 
-(defn false!
+(defn always-false
   "Takes a function of no arguments, and returns a function taking no arguments
   which calls f and returns false. f has presumably side effects."
   [f]
