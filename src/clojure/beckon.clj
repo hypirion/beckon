3b3ba2dc7038a456f97155ee4618f23463d08204
Jean Niklas L'orange <jeannikl@hypirion.com>
2013-07-14 19:16:04 +0200

    Add mockup of beckon.

--- src/clojure/beckon.clj
+++ src/clojure/beckon.clj
@@ -1,1 +1,21 @@
-(ns beckon)
+(ns beckon
+  (:import [com.hypirion.beckon SignalAtoms]
+           [sun.misc Signal]))
+
+(defn signal-atom [signal-name]
+  (SignalAtoms/getSignalAtom signal-name))
+
+(defn raise! [signal-name]
+  (Signal/raise (Signal. signal-name)))
+
+(defn true!
+  "Takes a function of no arguments, and returns a function taking no arguments
+  which calls f and returns true. f has presumably side effects."
+  [f]
+  (fn [] (f) true))
+
+(defn false!
+  "Takes a function of no arguments, and returns a function taking no arguments
+  which calls f and returns false. f has presumably side effects."
+  [f]
+  (fn [] (f) true))
