7c0f1d9e61305e40cb6e38464eb1e8496bb0d1e1
Jean Niklas L'orange <jeannikl@hypirion.com>
2013-07-19 23:11:00 +0200

    Fix documentation.

--- src/clojure/beckon.clj
+++ src/clojure/beckon.clj
@@ -8,18 +8,16 @@
   same (identical) atom.
 
   A beckon atom is an atom containing a Seqable Clojure collection, where all
-  the elements in the Seqable collection must be Callable. Clojure functions are
-  by default Callable if they take zero arguments. By default would a beckon
-  atom be a Clojure List with the default signal handler wrapped within a
-  Callable.
+  the elements in the Seqable collection must be Runnable. Clojure functions are
+  by default Runnable if they can take zero arguments. By default would a beckon
+  atom be a Clojure Set with the default signal handler wrapped within a
+  Runnable. Note that with sets, the ordering of the Runnable is arbitrary; If
+  you need ordered calling, convert the set to a vector or list first.
 
-  The signal handling of a (modified) beckon atom is as follows: The first
-  element in the sequence is called. If the element returns a truthy value, the
-  next element will be called in the same fashion. If the element returns a
-  falsey value or throws an Exception, the handling will stop and no more
-  elements will be called. A default signal handler will always return true
-  unless it throws an Exception. If the signal handler throws an Error, that
-  Error will not be caught.
+  The signal handling of a (modified) beckon atom is done by simply calling all
+  the functions in order. If the Runnable throws an Exception, the handling will
+  stop and no more elements will be called. If the signal handler throws an
+  Error, that Error will not be caught.
 
   signal-name must be a legal POSIX signal, where SIG is omitted from the first
   part of the name."
@@ -45,17 +43,3 @@
   "Reintializes all signal handlers handled by beckon to their default values."
   []
   (SignalRegisterer/resetAllHandlers))
-
-(defn always-true
-  "Takes a function of no arguments, and returns a function taking no arguments
-  which calls f and returns true. f has presumably side effects. Will not handle
-  throwables."
-  [f]
-  (fn [] (f) true))
-
-(defn always-false
-  "Takes a function of no arguments, and returns a function taking no arguments
-  which calls f and returns false. f has presumably side effects. Will not
-  handle throwables."
-  [f]
-  (fn [] (f) false))
