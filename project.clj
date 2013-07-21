9f5261c6d6092cf92adbbbf54602b1d711568b9a
Jean Niklas L'orange <jeannikl@hypirion.com>
2013-07-21 22:51:03 +0200

    Back to snapshot.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject beckon "0.1.0"
+(defproject beckon "0.2.0-SNAPSHOT"
   :description "Handle POSIX signals in Clojure with style and grace."
   :url "https://github.com/hyPiRion/beckon"
   :license {:name "Eclipse Public License"
