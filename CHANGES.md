53ff0423a07e3cf4df3f08f131b088ddc38f3f5e
Jean Niklas L'orange <jeannikl@hypirion.com>
2013-07-21 22:15:46 +0200

    Add in codox doc.

--- CHANGES.md
+++ CHANGES.md
@@ -2,9 +2,9 @@
 
 ## 0.1.0
 
-* New: The function `signal-atom` returns an atom containing a collection of
+* **New:** The function `signal-atom` returns an atom containing a collection of
   functions. The functions will be invoked sequentially whenever a signal is
   trapped.
-* New: The function `raise!` raises a signal.
-* New: `reinit!` and `reinit-all!` reinitializes signal handlers, and return
+* **New:** The function `raise!` raises a signal.
+* **New:** `reinit!` and `reinit-all!` reinitializes signal handlers, and return
   them to their "factory settings".
--- project.clj
+++ project.clj
@@ -1,8 +1,11 @@
 (defproject beckon "0.1.0-SNAPSHOT"
   :description "Handle POSIX signals in Clojure with style and grace."
-  :url "https://github.com/hyPiRion/com.hypirion.io"
+  :url "https://github.com/hyPiRion/beckon"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]]
   :source-paths ["src/clojure"]
   :java-source-paths ["src/java"]
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :profiles {:dev {:plugins [[codox "0.6.4"]]
+                   :codox {:sources ["src/clojure"]
+                           :output-dir "codox"}}})
