47006da7cddf30db030836e9a05c9b2542e00fcb
Jean Niklas L'orange <jeannikl@hypirion.com>
2013-08-30 20:34:25 +0200

    Don't deploy from nonstable.

--- project.clj
+++ project.clj
@@ -7,6 +7,7 @@
   :source-paths ["src/clojure"]
   :java-source-paths ["src/java"]
   :javac-options ["-target" "1.6" "-source" "1.6" "-Xlint:-options"]
+  :deploy-branches ["stable"]
   :profiles {:dev {:plugins [[codox "0.6.4"]]
                    :codox {:sources ["src/clojure"]
                            :output-dir "codox"}}})
